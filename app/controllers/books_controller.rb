020e5d21fb6a8d79feaa03e8f54e5a00803af3a5
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-03-21 14:34:22 +0000

    [Add]indexページ作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,40 @@
+class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to books_path(book.id)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def delete
+    book = Book.find(params[:id])
+    book.destroy
+    redirect _to 'books_path'
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+end
